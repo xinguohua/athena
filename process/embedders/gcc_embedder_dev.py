43be42f21c222be432330ebdeeb688c4f1af5897
xinguohua <794807072@qq.com>
2025-10-30 20:27:32 +0800

    打印

--- process/embedders/gcc_embedder_dev.py
+++ process/embedders/gcc_embedder_dev.py
@@ -165,7 +165,7 @@ class GCCEmbedderDev(GraphEmbedderBase):
         sem_fp_bits: int = 1024,
         # 恶意负样本与推开强度
         use_malicious_negatives: bool = True,
-        mal_neg_ratio: float = 0.8,
+        mal_neg_ratio: float = 0.3,
         mal_neg_token_len: int = 16,
         mal_stopwords=None,
             # [
@@ -180,7 +180,10 @@ class GCCEmbedderDev(GraphEmbedderBase):
     ):
         super().__init__(snapshots, features, mapp)
         if mal_stopwords is None:
-            mal_stopwords = []
+            mal_stopwords =  [
+            'event', 'read', 'write'
+            ,'execute'
+        ],
         self.snapshots = snapshots
         self.prop_feat_dim = int(prop_feat_dim)
         self.enc_hidden_dim = int(enc_hidden_dim)
